1da1787f4818e0b64bc22a747161dc7cff5442a4
andreafrank <andreajfrank23@gmail.com>
2020-08-14 22:57:41 -0500

    navbar styling

---
+++ src/components/about.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Container from 'bootstrap/dist/css/bootstrap.min.css';
+// import {  } from 'react-bootstrap';
+
+const About = () => { 
+	return (
+		<div>
+			Software engineering / UX
+
+			Other stuff I'm interested in
+
+		</div>
+	)
+}
+
+
+
+export default About;
---
+++ src/components/navbar.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './navbar.css';
+import { Navbar, Nav, NavDropdown, Form, FormControl, Button } from 'react-bootstrap';
+
+const NavBar = () => {
+	return (
+	    <Navbar className="color-nav" expand="lg">
+	  		<Navbar.Brand style={{color: '#dc136c', fontWeight: 'bold'}} href="#home">Andrea Frank</Navbar.Brand>
+	  		<Navbar.Toggle aria-controls="basic-navbar-nav" />
+	  		<Navbar.Collapse id="basic-navbar-nav">
+		    	<Nav className="mr-auto">
+		      	<Nav.Link style={{color: '#abff4f'}} href="#home">About</Nav.Link>
+		      	<Nav.Link style={{color: '#abff4f'}} href="#link">Projects</Nav.Link>
+		      	<Nav.Link style={{color: '#abff4f'}} href="#link">Résumé</Nav.Link>
+			      	<NavDropdown title="Blogs" id="green-color">
+			        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+			        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+			        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+			        <NavDropdown.Divider />
+			        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+			      	</NavDropdown>
+		   		</Nav>
+		   		<Form inline>
+			      	<FormControl type="text" placeholder="Search" className="mr-sm-2" />
+			      	<Button style={{color: '#abff4f'}} variant="outline-success">Coming Soon</Button>
+		    	</Form>
+		</Navbar.Collapse>
+		</Navbar>
+	)
+}
+
+export default NavBar;
