e4029b21aaf419ab0194152df1ed262ec364c982
andreafrank <andreajfrank23@gmail.com>
2020-08-15 11:40:22 -0500

    add about text

--- src/components/about.js
+++ src/components/about.js
@@ -7,11 +7,13 @@ const About = () => {
 	return (
 		<Card.Body>
 			<Card.Text>
-			Software engineering / UX
-
-			Other stuff I'm interested in
-
-			#100daysofcode thru  "this" site
+				Hi - I’m Andrea, and this website the result of my first day of #100DaysofCode. I’m adding new features and building this out every day so please visit back often. Up next is more React functionality and soon, some ThreeJS visuals. 
+				<br />
+				<br />
+				I attended DevBootcamp a few years ago before it closed (rip🥺) got a bad case of imposter syndrome and an unrelated job, and stopped programming. I enrolled in MICA’s MPS UX program earlier this year, and once I dipped my toes back into tech, ohhhhh did my passion for programming re-ignite. I’m finishing the program and graduate in May 2021, but am aligning my focus back to finding a full stack or front end position where I can both grow as a programmer and help interface with the UX folks and integrate teams. Hire me!  
+				<br />
+				<br />
+				In my free time, I have… a lot of hobbies. I like being outside, cooking (especially Thai food), sewing & pattern drafting, which, in terms of problem-solving and algorithmic thinking, has quite a lot in common with programming.
 			</Card.Text>
 		</Card.Body>
 	)
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -11,7 +11,7 @@ const NavBar = () => {
 	  		<Navbar.Toggle aria-controls="basic-navbar-nav" />
 	  		<Navbar.Collapse id="basic-navbar-nav">
 		    	<Nav className="mr-auto">
-		      	<Nav.Link style={{color: '#abff4f'}} href="#home">About</Nav.Link>
+		      	<Nav.Link style={{color: '#abff4f'}} href="#about">About</Nav.Link>
 		      	<Nav.Link style={{color: '#abff4f'}} href="#link">Projects</Nav.Link>
 		      	<Nav.Link style={{color: '#abff4f'}} href="#link">Résumé</Nav.Link>
 			      	<NavDropdown title="Blogs" id="green-color">
